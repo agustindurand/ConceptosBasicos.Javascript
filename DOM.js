579b750ff00d2e92ab90ce6680b8b7b620bc709a
Agustin Durand <agustindurand.98@gmail.com>
2023-07-27 17:22:30 -0300

    dom

---
+++ DOM.js
@@ -0,0 +1,9 @@
+// Document Objet Model // 
+// Se accede a los elementos HTML // 
+// Cada etiqueta de HTML es un objeto al que llamamos NODO, NODOS PADRE E NODOS HIJOS
+// Empleamos document (es el objeto global)//
+// console. dir() es la manera de ver todas las propiedades de un objeto JavaScript específicado por consola,//
+
+console.log(document.URL); // tE PERMITE VER LA URL DEL ARCHIVO //
+
+console.log(document.scripts); // TE PERMITE VER LA CANTIDAD DE SCRIPTS DEL CODIGO //
