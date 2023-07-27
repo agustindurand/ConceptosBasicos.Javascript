510442b93728d8a6692281c92c9d232d489ecb0b
Agustin Durand <agustindurand.98@gmail.com>
2023-07-27 17:55:42 -0300

    dom 2

--- DOM.js
+++ DOM.js
@@ -7,3 +7,11 @@
 console.log(document.URL); // tE PERMITE VER LA URL DEL ARCHIVO //
 
 console.log(document.scripts); // TE PERMITE VER LA CANTIDAD DE SCRIPTS DEL CODIGO //
+
+// Metodos para acceder elementos del dom utilizando el Document //
+// getElementByID, getElementsByClassName, getElementsByTagName // 
+
+const mih1 = document.getElementById('uno'); //Si encuentra el elemento lo retorna, de lo contrario devuelve null //
+console.log(mih1); // Nodo de texto //
+console.log(mih1.textContent);
+mih1.textContent = "My Shopping card";
